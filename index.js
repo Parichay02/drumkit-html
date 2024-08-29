b4622c946df6996a41bc0d5ab5582aad8ece1d7e
Parichay02 <130536873+Parichay02@users.noreply.github.com>
2024-08-30 04:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function makesound(key)
     
         case 'w':
         
-            var crash=new Audio("sounds/crash.mp3");
+            var crash=new Audio("crash.mp3");
             crash.play();
             break;
         
@@ -33,7 +33,7 @@ function makesound(key)
             case 'a':
                
         
-                    var kickbass=new Audio("sounds/kick-bass.mp3");
+                    var kickbass=new Audio("kick-bass.mp3");
                     kickbass.play();
                     
                 
@@ -42,14 +42,14 @@ function makesound(key)
             case 's':
                
         
-                    var snare=new Audio("sounds/snare.mp3");
+                    var snare=new Audio("snare.mp3");
                     snare.play();
                     
                 
             break;
             case 'd':
         
-                var tom1=new Audio("sounds/tom-1.mp3");
+                var tom1=new Audio("tom-1.mp3");
                 tom1.play();
                 
             
@@ -57,7 +57,7 @@ function makesound(key)
             break;
             case 'j':  
         
-                var tom2=new Audio("sounds/tom-2.mp3");
+                var tom2=new Audio("tom-2.mp3");
                 tom2.play();
                 
             
@@ -66,7 +66,7 @@ function makesound(key)
             case 'k':
                 
         
-                    var tom3=new Audio("sounds/tom-3.mp3");
+                    var tom3=new Audio("tom-3.mp3");
                     tom3.play();
                     
                 
@@ -74,7 +74,7 @@ function makesound(key)
             break;
             case 'l':
         
-                var tom4=new Audio("sounds/tom-4.mp3");
+                var tom4=new Audio("tom-4.mp3");
                 tom4.play();
                 
             
